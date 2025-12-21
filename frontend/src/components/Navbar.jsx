df9f34551356a5bf70863549444e5edf5b4f7c73
raimundGage <raimundgage@outlook.com>
2025-12-21 11:03:17 +0800

    Replace the tab icon and upgrade the Navbar.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 const Navbar = () => {
     return (
-        <nav>
-            <div className="">
-            useful navigation!
+        <nav className="navbar">
+            <h1>my site ^^</h1>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/about">about</a>
+                <a href="/add">add</a>
             </div>
         </nav>
     );
