00e42e17acf9dd57c831f0969b64cd0503d77b42
raimundGage <raimundgage@outlook.com>
2025-12-26 14:17:15 +0800

    practicing more and more in creating the old window as a component ^^

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,38 @@
 import { Button } from './pieces/Button';
 import { Marquee } from './pieces/Marquee';
 import { RetroWindow } from './components/RetroWindow';
-import { Window } from './components/Window';
+import { Window} from './components/Window';
 
 function App() {
   const favorites = [
-    {icon: "🐯", name: "Grantly Bell",    from:             "Knights College"},
+    {icon: "🐯", name: "Grantly Bell",    from: "Knights College"            },
     {icon: "🐺", name: "Albert",          from: "Intermarché - Conte de Noël"},
-    {icon: "🐲", name: "Cold Snapdragon", from: "Plants VS Zombies 2"},
+    {icon: "🐲", name: "Cold Snapdragon", from: "Plants VS Zombies 2"        },
   ];
 
   return (
     <article className="App">
+
       <section>
+        {/* Scrolling Marquee Of My Faves */}
         <div>
           {favorites.map((favs) => {
-            return <Marquee icon={favs.icon} name={favs.name} from={favs.from} />
+            return <Marquee key={favs.name} icon={favs.icon} name={favs.name} from={favs.from} />
           })}
         </div>
-
+        
+        {/* Button */}
         <Button buttonText={"mrowwww"}/>
       </section>
 
+      {/* Load The Image Inside The Window Component */}
       <RetroWindow title={"some_cats"} imageSource={"https://pbs.twimg.com/media/G803GDrb0AA6H-M?format=jpg&name=large"} />
-          
-      <div className='mt-10'/>
-      <Window title={""}/>
+      
+      {/* Practicing To Replicate The Old Window */}
+      <div className='mt-3'>
+        <Window title={"Who Am I?"} content={"I'm Raimund Gage!"}/>
+      </div>
+
     </article>
   )
 }
--- frontend/src/components/Window.jsx
+++ frontend/src/components/Window.jsx
@@ -1,17 +1,17 @@
 export const Window = ({ title, content }) => {
     return (
         <section>
-            <div className="bg-win-grey p-1 border-2 max-w-sm
-                            shadow-[1px_1px_0_0_#000000]
-                            border-l-win-light border-t-win-light
-                            border-r-win-dark border-b-win-dark">
-
+            
+            <div className="bg-win-grey p-1 max-w-2xl border-2 shadow-[1px_1px_0_0_#000000]
+                            border-l-win-light border-t-win-light border-r-win-dark border-b-win-dark">
+                
                 {/* Title */}
-                <div className="bg-win-blue px-2 py-1 justify-between items-center mb-2">
-                    <span className="text-white font-bold text-sm tracking-wide">{title}</span>
+                <div className="bg-win-blue px-2 py-1 mb-2 flex justify-between items-center">
+                    <span className="text-white">{title}</span>
                     <div className="flex gap-1">
-                        <button className="bg-win-grey border border-l-win-light border-t-win-light border-r-black border-b-black px-1 text-xs leading-none font-bold">A</button>
-                        <button className="bg-win-grey border border-l-win-light border-t-win-light border-r-black border-b-black px-1 text-xs leading-none font-bold">A</button>
+                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">?</button>
+                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">-</button>
+                        <button className="px-1 text-xs leading-none font-bold bg-win-grey border border-l-white border-t-white border-r-black border-b-black">X</button>
                     </div>
                 </div>
 
@@ -21,6 +21,7 @@ export const Window = ({ title, content }) => {
                 </div>
 
             </div>
+
         </section>
     );
 };
