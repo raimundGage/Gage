8b9f403445181e9b25e7be457d8b46129142fa6e
raimundGage <raimundgage@outlook.com>
2025-12-29 11:43:12 +0800

    fixing the scrolling marquee animation regardless of screen size ^^

--- frontend/src/pieces/Marquee.jsx
+++ frontend/src/pieces/Marquee.jsx
@@ -1,7 +1,7 @@
 export const Marquee = ({ icon, name, from }) => {
     return(
         <section>
-            <div className="bg-black overflow-hidden whitespace-nowrap my-2 cursor-help">
+            <div className="@container bg-black overflow-hidden whitespace-nowrap my-2 cursor-help">
                 <div className="text-neon-green inline-block font-serif animate-marquee">
                     +-+ {icon} {name} from {from} +-+
                 </div>
