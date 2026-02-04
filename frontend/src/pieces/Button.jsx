0bc9c66854ffddaed48f9d8a492a553466a31b00
raimundGage <raimundgage@outlook.com>
2026-02-04 08:24:47 +0800

    familiarizing again with tailwind ^^

--- frontend/src/pieces/Button.jsx
+++ frontend/src/pieces/Button.jsx
@@ -2,8 +2,9 @@ export const Button = ({ buttonLabel }) => {
   return (
     <>
       <button
-        className="bg-win-grey px-1.5 active:border-l-win-dark active:border-t-win-dark active:border-r-win-light active:border-b-win-light
-                   border-2 border-l-win-light border-t-win-light border-r-win-dark border-b-win-dark max-w-fit">
+        className="bg-win-grey px-2 py-1 active:border-l-win-dark active:border-t-win-dark active:border-r-win-light active:border-b-win-light
+                   border-2 border-l-win-light border-t-win-light border-r-win-dark border-b-win-dark max-w-fit
+                  font-bold active:text-win-blue">
 
         {buttonLabel}
 
